925a86bb61dd4ad2182d74c0a97901845d8cf9dd
wttwhite <wangtingting@jxhsja.com>
2023-02-09 18:12:59 +0800

    feat: 页面

---
+++ pages/index/const.js
@@ -0,0 +1,65 @@
+export const AllList = [
+  { id: "1", name: "类型一" },
+  { id: "2", name: "类型二" },
+  { id: "3", name: "类型三" },
+  { id: "4", name: "类型四" },
+  { id: "5", name: "类型五" },
+  { id: "6", name: "类型六" },
+];
+export const RightData = {
+  1: [
+    {
+      id: "11",
+      name: "aaa",
+      img: "",
+      material: "xxx 500g、xxx 100g、xxx 500g、xxx 100g",
+      seasoning: "xxx 500g、xxx 100g",
+      step: ["1.xxxx", "2.xxxx"],
+    },
+  ],
+  2: [
+    {
+      id: "22",
+      name: "bbb",
+      img: "",
+      material: "xxx 500g",
+      step: ["1.xxxx", "2.xxxx"],
+    },
+  ],
+  3: [
+    {
+      id: "33",
+      name: "vvv",
+      img: "",
+      material: "xxx 500g",
+      step: ["1.xxxx", "2.xxxx"],
+    },
+  ],
+  4: [
+    {
+      id: "44",
+      name: "eee",
+      img: "",
+      material: "xxx 500g",
+      step: ["1.xxxx", "2.xxxx"],
+    },
+  ],
+  5: [
+    {
+      id: "55",
+      name: "555",
+      img: "",
+      material: "xxx 500g",
+      step: ["1.xxxx", "2.xxxx"],
+    },
+  ],
+  6: [
+    {
+      id: "66",
+      name: "222",
+      img: "",
+      material: "xxx 500g",
+      step: ["1.xxxx", "2.xxxx"],
+    },
+  ],
+};
